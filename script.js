f96c6e2906433f5589c28439a8ef82ea22441f38
Tasfia Nowrin <nowirntasfia157@gmail.com>
2025-12-21 21:40:23 -0700

    Connected contact form with Google Sheet

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", function (e) {
   const email = form.email.value;
   const message = form.message.value;
 
-  fetch("https://script.google.com/macros/s/AKfycby32rIASSfYGoR2LUv33-C5gtzEN0SEcJi2Ioa60Y9peK8LiJiXYI4knkKqswYZDDmazQ/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbyA_HQmr3PJy4IEfGj7kPMOSFPKy4-zTlZF1xP5EnkL5yLDbcA7MofN36aNgrSVs5BStQ/exec", {
     method: "POST",
     body: JSON.stringify({
       name: name,
