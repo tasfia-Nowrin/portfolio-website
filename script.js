ceb6dd9d2fcd0c2967429de1e93c8ddf34d67b08
Tasfia Nowrin <nowirntasfia157@gmail.com>
2025-12-21 21:56:59 -0700

    Update script page

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbxaV9h1Se1jKC5hmLgAx65AzZ7Nw33YVbyZ-ABQIawZ7zi0kzLHdAQIT6-yrr8sfgwIIA/exec";
 const form = document.getElementById("contactForm");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", e => {
   e.preventDefault();
 
-  const name = form.name.value;
-  const email = form.email.value;
-  const message = form.message.value;
+  const data = {
+    name: form.name.value,
+    email: form.email.value,
+    message: form.message.value
+  };
 
-  fetch("https://script.google.com/macros/s/AKfycbyA_HQmr3PJy4IEfGj7kPMOSFPKy4-zTlZF1xP5EnkL5yLDbcA7MofN36aNgrSVs5BStQ/exec", {
+  fetch(scriptURL, {
     method: "POST",
-    body: JSON.stringify({
-      name: name,
-      email: email,
-      message: message
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
+    body: JSON.stringify(data)
   })
-  .then(res => res.text())
-  .then(data => {
+  .then(res => res.json())
+  .then(response => {
     alert("Message sent successfully!");
     form.reset();
   })
-  .catch(err => {
+  .catch(error => {
     alert("Error sending message");
-    console.error(err);
+    console.error(error);
   });
 });
